e8475b6235cbe9a63935a297b31dcbc2d4534fbb
seymour-wav <114354761+seymour-wav@users.noreply.github.com>
2025-11-03 12:16:51 -0500

    Update UiObserver.java

--- UiObserver.java
+++ UiObserver.java
@@ -1,1 +1,11 @@
+public interface UiObserver {
+  // Update pushes data
+  public update(UiUpdate update) {
 
+  }
+
+  // onNotified pulls data
+  public onNotified (UiSubjectRef ref, Channel ch) {
+
+  }
+}
