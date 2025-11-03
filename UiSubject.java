779b7a19f9a60a753e544ed886e07af0f51c04e7
seymour-wav <114354761+seymour-wav@users.noreply.github.com>
2025-11-03 12:46:58 -0500

    Update UiSubject.java

--- UiSubject.java
+++ UiSubject.java
@@ -4,21 +4,17 @@ import java.util.*;
    * Registers an observer from one or more channel.
    */
 public interface UiSubject {
-  void register(UiObserver o, Channel ch) {
-  
-  }
+  void register(UiObserver o, Channel ch);
 
   /**
    * Unregisters an observer from one or more channel.
    */
-  void unregister(UiObserver o, Channel ch) {
-  
-  }
+  void unregister(UiObserver o, Channel ch);
 
 /**
  * Notify observer subscribed to a given channel.
  */
-  void notify(Channel ch) {
-  
-  }
+  void notify(Channel ch);
+
+  boolean modeToggle();
 }
